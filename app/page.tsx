202014a81c93041a47da4ec39d7b3bef5c4241e3
txmpold <love.tampold@hotmail.com>
2026-09-14 18:18:06 +0200

    changed call to action button

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
   <h1 className="absolute top-75 left-1/2 -translate-x-1/2 text-[#EDE7DA] font-extrabold lg:text-9xl text-center tracking-tight drop-shadow-lg select-none">
     PLAY LIKE IT'S 1989.
   </h1>
-  <button className="absolute py-3 px-10 rounded-lg text-white font-bold bg-[#C4302B] lg:bottom-50 left-1/2 -translate-x-1/2 translate-y-1/2 hover:cursor-pointer hover:bg-[#EFA61D] hover:text-black transition-all duration-300">
+  <button className="absolute py-3 px-10 rounded-lg text-white font-bold bg-[#b8342a] lg:bottom-50 left-1/2 -translate-x-1/2 translate-y-1/2 hover:cursor-pointer hover:bg-black transition-all duration-300">
     <Link href="/product" className="text-lg">
       Shop Now
     </Link>
