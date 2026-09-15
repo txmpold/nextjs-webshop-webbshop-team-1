9b8aafc5c03ef68c8aa51a036bfa3e4e4feda3f7
txmpold <love.tampold@hotmail.com>
2026-09-15 14:40:24 +0200

    change metadata to new name and english

--- app/product/[articleNumber]/[slug]/page.tsx
+++ app/product/[articleNumber]/[slug]/page.tsx
@@ -51,19 +51,16 @@ export default async function ProductDetailPage({
 
             <DetailPageDropdown
               title="Shipping"
-              content="We aim to dispatch all orders within 24 business hours. Worn Stories offers UPS and Postnord shipping services.\nThe shipping service and cost is based on your selected location. We offer free shipping worldwide on all orders over 250€."
+              content="We aim to dispatch all orders within 24 business hours. Kassetten offers UPS and Postnord shipping services.\nThe shipping service and cost is based on your selected location. We offer free shipping worldwide on all orders over 500kr."
             />
             <DetailPageDropdown
               title="Returns"
-              content="Worn Stories has a 14-day return policy: you have 14 days from
+              content="Kassetten has a 14-day return policy: you have 14 days from
               when your order is delivered to ship it back to us for a refund or
               exchange.​ The order must be returned in original condition with
               included original packaging."
             />
-            <DetailPageDropdown
-              title="Care for your vintage items"
-              content="You can machine wash most cotton & cotton blend products in 40 degrees celcius / 104 degrees fahrenheit. Wool products should preferably be hand washed. Machine wash at your own risk. Remember that the items we offer have been used previously and that some are more fragile (depending on age and condition). Always have that in mind before washing. Vintage leather products should hang in dry areas and should be conditioned regularly to avoid cracks. Note that we do not take responsibility for any product that got damaged when worn or washed."
-            />
+
             <p className="text-xs text-gray-500" data-cy="product-id">
               Article number: {product.articleNumber}
             </p>
